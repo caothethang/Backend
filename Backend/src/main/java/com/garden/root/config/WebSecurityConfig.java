0c20229ccfe09c020069aa4253142868a52fd300
Thang Cao <thangct@blockchaindeveloper.asia>
2021-12-20 20:08:29 +0700

    fix cors

---
+++ Backend/src/main/java/com/garden/root/config/WebSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.garden.root.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().configurationSource(request -> corsConfiguration())
+                .and().csrf().disable();
+    }
+    
+    @Bean
+    CorsConfiguration corsConfiguration() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.applyPermitDefaultValues();
+        corsConfiguration.addAllowedMethod(HttpMethod.GET);
+        corsConfiguration.addAllowedMethod(HttpMethod.POST);
+        corsConfiguration.addAllowedMethod(HttpMethod.PATCH);
+        corsConfiguration.addAllowedMethod(HttpMethod.PUT);
+        corsConfiguration.addAllowedMethod(HttpMethod.DELETE);
+        return corsConfiguration;
+    }
+}
--- Backend/src/main/java/com/garden/root/controller/ImageController.java
+++ Backend/src/main/java/com/garden/root/controller/ImageController.java
@@ -9,7 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin
 @RequestMapping("/api/images")
 public class ImageController {
     
