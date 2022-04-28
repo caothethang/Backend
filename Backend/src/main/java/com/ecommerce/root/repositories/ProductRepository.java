1fbc09779ca9232001f23ac7452694fef343a7dd
thangct <caothethangnvx@gmail.com>
2022-04-28 23:30:00 +0700

    g

--- Backend/src/main/java/com/ecommerce/root/repositories/ProductRepository.java
+++ Backend/src/main/java/com/ecommerce/root/repositories/ProductRepository.java
@@ -3,11 +3,13 @@ package com.ecommerce.root.repositories;
 import com.ecommerce.root.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long>, JpaSpecificationExecutor<Product> {
 
-    @Override
-    void deleteById(Long aLong);
+    @Query(value = "delete from tbl_product where id = :id",nativeQuery = true)
+    void deleteById(@Param("id") Long id);
 }
